397e76d2519d3e8c66b68e28e192a21a627e0986
Eswar2209 <2403031460110@paruluniversity.ac.in>
2026-06-03 12:03:29 +0530

    Time: 23 ms (12.04%), Space: 22.7 MB (8.73%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,16 +1,22 @@
 class Solution:
-    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        d = {} 
+    def findDiagonalOrder(self, mat):
+        d = {}
+
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if i + j not in d:
                     d[i + j] = []
+
                 d[i + j].append(mat[i][j])
 
-        arr = []
-        for k in d:
+        ans = []
+
+        for k in range(len(d)):
             if k % 2 == 0:
-                arr.extend(d[k][::-1])
+                for x in range(len(d[k]) - 1, -1, -1):
+                    ans.append(d[k][x])
             else:
-                arr.extend(d[k])
-        return arr
+                for x in d[k]:
+                    ans.append(x)
+
+        return ans
